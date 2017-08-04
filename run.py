ee0b42ac0acca99f99721259d9b0ca598e2fb94d
mii <mii012345@outlook.jp>
2017-08-04 22:45:31 +0900

    fix run

--- run.py
+++ run.py
@@ -1,21 +1,22 @@
 import numpy as np
 from files import mnist
-from files import neural_three_layer as neural
+#from files import neural_three_layer as neural
 import os.path as p
 import pickle
-(x_train,t_train),(x_test,t_test) = mnist.load_mnist(normalize=True,one_hot_label=True)
-network = neural.Neural(input_size=784,hidden_size=1000,output_size=10)
+(x_train,t_train),(x_test,t_test) = mnist.load_mnist(normalize=True,flatten=False,one_hot_label=True)
+print(x_train.shape)
+#network = neural.Neural(input_size=784,hidden_size=100,output_size=10)
 '''
 input_sizeは入力の数に合わせてください。
 hidden_sizeは隠れ層のニューロンの数です。お好きな数字でどうぞ
 output_sizeは出力の数です。今回は数字識別なので10です。
-'''
-if p.exists("params.pkl") == True:
-    network.load_params("params.pkl")
+
+if p.exists("params0.pkl"):
+    network.load_params("params0.pkl")
 
 iters_num = 10000 #回数を指定します
 train_size = x_train.shape[0]
-batch_size = 10 #一度に学習するデータの数を指定します
+batch_size = 100 #一度に学習するデータの数を指定します
 learning_rate = 0.1 #学習率
 
 for i in range(iters_num):
@@ -37,6 +38,7 @@ for i in range(iters_num):
 
 
 
-a=network.accuracy(x_train,t_train)
+a=network.accuracy(x_test,t_test)
 print(a)
-network.save_params("params.pkl")
+network.save_params("params0.pkl")
+'''
