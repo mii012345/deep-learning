660785157446583eefeefa9d5dc25927aab6a9e4
mii <mii012345@outlook.jp>
2017-08-18 11:05:18 +0900

    ok

--- files/neural_layer.py
+++ files/neural_layer.py
@@ -1,14 +1,17 @@
 import sys,os
 sys.path.append(os.pardir)
 import cupy as cp
+import numpy as np
 from . import layers as l
 from  collections import OrderedDict
+import gc
+import pickle
 
 class NeuralNet:
     def __init__(self,input_dim=(3,360,360),
                  conv_param={'filter_num':30,'filter_size':5,
                              'pad':0,'stride':1},
-                 hidden_size=100,output_size=3,weight_init_std=0.01):
+                 hidden_size=50,output_size=3,weight_init_std=0.01):
         filter_num = conv_param['filter_num']
         filter_size = conv_param['filter_size']
         filter_pad = conv_param['pad']
@@ -16,25 +19,34 @@ class NeuralNet:
         input_size = input_dim[1]
         conv_output_size = (input_size-filter_size+2*filter_pad)/filter_stride+1
         pool_output_size = int(filter_num*(conv_output_size/2)*(conv_output_size/2))
+        self.lr =0.1
 
         self.params = {}
-        self.params['W1'] = weight_init_std * cp.random.randn(filter_num,input_dim[0],filter_size,filter_size)
-        self.params['b1'] = cp.zeros(filter_num)
-        self.params['W2'] = weight_init_std * cp.random.randn(pool_output_size,hidden_size)
-        self.params['b2'] = cp.zeros(hidden_size)
-        self.params['W3'] = weight_init_std * cp.random.randn(hidden_size,output_size)
-        self.params['b3'] = cp.zeros(output_size)
+        self.params['W1'] = weight_init_std * np.random.randn(filter_num,input_dim[0],filter_size,filter_size)
+        self.params['b1'] = np.zeros(filter_num)
+        self.params['W2'] = weight_init_std * np.random.randn(pool_output_size,hidden_size)
+        self.params['b2'] = np.zeros(hidden_size)
+        self.params['W3'] = weight_init_std * np.random.randn(hidden_size,output_size)
+        self.params['b3'] = np.zeros(output_size)
 
         self.layers = OrderedDict()
-        self.layers['Conv1'] = l.Convolution(self.params['W1'],self.params['b1'],conv_param['stride'],conv_param['pad'])
+        params_gpu = {}
+        for key in ['W1','b1','W2','b2','W3','b3']:
+            params_gpu[key] = cp.array(self.params[key])
+        self.layers['Conv1'] = l.Convolution(params_gpu['W1'],params_gpu['b1'],conv_param['stride'],conv_param['pad'])
         self.layers['Relu1'] = l.Relu()
         self.layers['Pool1'] = l.Pooling(pool_h=2,pool_w=2,stride=2)
-        self.layers['Affine1'] = l.Affine(self.params['W2'],self.params['b2'])
+        self.layers['Affine1'] = l.Affine(params_gpu['W2'],params_gpu['b2'])
         self.layers['Relu2'] = l.Relu()
-        self.layers['Affine2'] = l.Affine(self.params['W3'],self.params['b3'])
+        self.layers['Affine2'] = l.Affine(params_gpu['W3'],params_gpu['b3'])
+        self.layers['Dropout'] = l.Dropout()
+
         self.lastLayer = l.SoftmaxWithLoss()
 
-        self.opt = l.AdaGrad()
+        self.opt = l.AdaGrad(lr=self.lr)
+
+        del params_gpu
+        gc.collect()
 
     def predict(self,x):
         for layer in self.layers.values():
@@ -78,3 +90,20 @@ class NeuralNet:
     def update(self,x,t):
         grad = self.gradient(x,t)
         self.opt.update(self.params,grad)
+
+    def save_params(self, file_name="params.pkl"):
+        params = {}
+        for key, val in self.params.items():
+            params[key] = val
+        with open(file_name, 'wb') as f:
+            pickle.dump(params, f)
+
+    def load_params(self, file_name="params.pkl"):
+        with open(file_name, 'rb') as f:
+            params = pickle.load(f)
+        for key, val in params.items():
+            self.params[key] = val
+
+        for i, key in enumerate(['Conv1', 'Affine1', 'Affine2']):
+            self.layers[key].W = cp.array(self.params['W' + str(i+1)])
+            self.layers[key].b = cp.array(self.params['b' + str(i+1)])
--- train and test.py
+++ train and test.py
@@ -1,61 +1,152 @@
 import cupy as cp
-from files import mnist
+import numpy as np
 from files import neural_layer as neural
 from files import tenki_num as t
-import gc
 import chainer.cuda as cuda
 import time
+import gc
+import matplotlib.pyplot as plt
+
+def testNeural(network,x_test,t_test):
+    print("Start Test")
+    ans = 0
+    for i in np.arange(0,200,5):
+        a = [i]
+        x_batch = x_test[a]
+        t_batch = t_test[a]
+        print(i,end=" ")
+        for ii in range(1,5):
+            print(i+ii,end=" ")
+            x_batch = np.append(x_batch,x_test[[i+ii]],axis=0)
+            t_batch = np.append(t_batch,t_test[[i+ii]],axis=0)
+        x_batch = cp.array(x_batch)
+        t_batch = cp.array(t_batch)
+
+        acc = network.accuracy(x_batch,t_batch)
+        #network.update(x_batch,t_batch)
+        print(acc)
+        if acc == 1.0:
+            ans += 5
+        #if acc == 0.5:
+            #ans += 1
+        if acc == 0.8:
+            ans += 4
+        if acc == 0.6:
+            ans += 3
+        if acc == 0.4:
+            ans += 2
+        if acc == 0.2:
+            ans += 1
+
+    print()
+    print(ans,end=" ")
+    ansper = (ans/200)*100
+    print(ansper)
+    return ansper
 
 print("Now Loading...")
-mas,tenki,mastest,tenkitest = t.loadPic()
-mas = mas.astype(cp.float32)
-mas /= 255.0
-x_train = mas[:350]
-t_train = tenki[:350]
-x_test = mastest[:200]
-t_test = tenkitest[:200]
-A = time.time()
+x_train,t_train,x_test,t_test,x_acc,t_acc = t.loadPic()
 
-del mas,tenki,mastest,tenkitest
-gc.collect()
+x_train = x_train.astype(np.float32)
+x_test = x_test.astype(np.float32)
+x_acc = x_acc.astype(np.float32)
+
+acc_list = []
+ar = 2
 
 network = neural.NeuralNet()
+train_num = int(input("train size"))
+boo = bool(input("load params?"))
+if boo:
+    network.load_params("params.pkl")
+A = time.time()
+
 ans = 0
 print("Start Train")
-for i in range(350):
-    a = [i]
-    x_batch = x_train[a]
-    t_batch = t_train[a]
-    grad = network.gradient(x_batch,t_batch)
-    for key in ('W1','b1','W2','b2','W3','b3'):
-        network.params[key] -= 0.01 * grad[key]
-    #network.update(x_batch,t_batch)
+for i in range(0,train_num,ar):
+    #x = random.randint(0,699)
+    #a = [x]
+    #a = [i]
 
-    y = network.predict(x_batch)
-    if cp.argmax(y[0,:]) == cp.argmax(t_batch[0,:]):
-        answer = "〇"
-    else:
-        answer = "×"
-    print(i,"seikai",cp.argmax(t_batch[0,:]),"yosoku",cp.argmax(y[0,:]),answer)
-B = time.time()
-print(B-A)
-#for key in ('W1','b1','W2','b2','W3','b3'):
-    #network.params[key] = cuda.to_cpu(network.params[key])
-    #print(type(network.params[key]))
+    batch_mask = np.random.choice(x_train.shape[0]-1,ar)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    #x_batch = x_train[a]
+    #t_batch = t_train[a]
+    #for ii in range(1,2):
+        #x_batch = np.append(x_batch,x_train[[i+ii]],axis=0)
+        #t_batch = np.append(t_batch,t_train[[i+ii]],axis=0)
+    x_batch = cp.array(x_batch)
+    t_batch = cp.array(t_batch)
+    network.update(x_batch,t_batch)
+    del x_batch,t_batch
+    gc.collect()
+    print(i)
+    if i>0 and i%100 == 0:
+        ansper = testNeural(network,x_test,t_test)
+        acc_list.append(ansper)
 print("Start Test")
-for i in range(200):
+ans = 0
+for i in np.arange(0,300,5):
     a = [i]
-    x_batch = x_test[a]
-    t_batch = t_test[a]
-    #network.update(x_batch,t_batch)
+    x_batch = x_acc[a]
+    t_batch = t_acc[a]
+    print(i,end=" ")
+    for ii in range(1,5):
+        print(i+ii,end=" ")
+        x_batch = np.append(x_batch,x_acc[[i+ii]],axis=0)
+        t_batch = np.append(t_batch,t_acc[[i+ii]],axis=0)
+    x_batch = cp.array(x_batch)
+    t_batch = cp.array(t_batch)
 
-    y = network.predict(x_batch)
-    if cp.argmax(y[0,:]) == cp.argmax(t_batch[0,:]):
-        answer = "〇"
+    acc = network.accuracy(x_batch,t_batch)
+    #network.update(x_batch,t_batch)
+    print(acc)
+    if acc == 1.0:
+        ans += 5
+    if acc == 0.8:
+        ans += 4
+    if acc == 0.6:
+        ans += 3
+    if acc == 0.4:
+        ans += 2
+    if acc == 0.2:
         ans += 1
-    else:
-        answer = "×"
-    print(i,"seikai",cp.argmax(t_batch[0,:]),"yosoku",cp.argmax(y[0,:]),answer)
-print((ans/200)*100)
-#300,200->47.5
+
+print()
+print(ans,end=" ")
+ansper = (ans/300)*100
+print(ansper)
+acc_list.append(ansper)
+
+
+B = time.time()
+tim = int(np.round((B-A)/60))
+print(tim,"min")
+boo = bool(input("save params?"))
+if boo:
+    network.save_params("params_gpu.pkl")
+
+x = np.arange(100,train_num+1,100)
+plt.plot(x, acc_list,marker='o')
+plt.xlabel("many")
+plt.ylabel("accuracy")
+plt.title(str(tim)+"min")
+plt.ylim(0, 100.0)
+plt.show()
+
+#300,200->42.0
 #350,200->50.5
+#400,200->50.5
+#450,200->50.5
+#500,200->50.5
+#550,200->39.0
+#600,200->38.5
+
+
+#700,200->50.5
+
+#Momentum&hidden=50
+#650,200->38.5
+#700,200->38.5
